a20ef699aa77609622c5dc1de2f0c76066ca8011
NileshGupta1981 <nileshgupta1981@gmail.com>
2016-04-26 21:55:59 +0530

    Session4Assignment3

---
+++ Employee.java
@@ -0,0 +1,34 @@
+public class Employee {
+	
+	private int EmpID;
+	private String EmpName;
+	private int EmployeeAge;
+	private String Dept;
+	public int getEmpID() {
+		return EmpID;
+	}
+	public void setEmpID(int empID) {
+		EmpID = empID;
+	}
+	public String getEmpName() {
+		return EmpName;
+	}
+	public void setEmpName(String empName) {
+		EmpName = empName;
+	}
+	public int getEmployeeAge() {
+		return EmployeeAge;
+	}
+	public void setEmployeeAge(int employeeAge) {
+		EmployeeAge = employeeAge;
+	}
+	public String getDept() {
+		return Dept;
+	}
+	public void setDept(String dept) {
+		Dept = dept;
+	}
+	
+	
+	
+}
---
+++ Session4Assignment3.java
@@ -0,0 +1,26 @@
+public class Session4Assignment3{
+	
+	public static void main (String args[]){
+		Employee obj = new Employee();
+		obj.setEmpID(12345);
+		obj.setDept("HR");
+		obj.setEmployeeAge(25);
+		obj.setEmpName("Ram");
+		
+		System.out.println("Employee Details are");
+		System.out.println("Name:"+obj.getEmpName());
+		System.out.println("EmpID:"+obj.getEmpID());
+		System.out.println("Age:"+obj.getEmployeeAge());
+		System.out.println("Dept:"+obj.getDept());
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+}
